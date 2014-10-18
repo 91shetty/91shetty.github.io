c4c5c2e0c45a707bb9a9020df013a9159dc5b081
Vishwas Shetty <vishshetty1504@gmail.com>
2014-10-19 01:25:48 +0530

    adding javascript action

---
+++ sc/action.js
@@ -0,0 +1,17 @@
+function showDashboard{
+	document.getElementById('dashboard').style.display = 'block';
+	document.getElementById('users').style.display = 'none';
+	document.getElementById('setting').style.display = 'none';
+}
+
+function showUsers{
+	document.getElementById('dashboard').style.display = 'none';
+	document.getElementById('users').style.display = 'block';
+	document.getElementById('setting').style.display = 'none';
+}
+
+function showSetting{
+	document.getElementById('dashboard').style.display = 'none';
+	document.getElementById('users').style.display = 'none';
+	document.getElementById('setting').style.display = 'block';
+}
