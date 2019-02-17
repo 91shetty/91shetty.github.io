b4b03510b9fff3e4c3e044b0f4af6c5ced422e44
Vishwas Shetty <vishshetty1504@gmail.com>
2019-02-17 13:55:51 -0800

    Adding predictor project

---
+++ predictor/precache-manifest.0e833a4e3c99a7d50385c4102a8f2960.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "61da3c2d650c36c0d774",
+    "url": "/static/js/main.61da3c2d.chunk.js"
+  },
+  {
+    "revision": "539add5b2989134501bb",
+    "url": "/static/js/1.539add5b.chunk.js"
+  },
+  {
+    "revision": "61da3c2d650c36c0d774",
+    "url": "/static/css/main.319f6c1f.chunk.css"
+  },
+  {
+    "revision": "a34bac6879723e544294266fd11bc976",
+    "url": "/index.html"
+  }
+];
